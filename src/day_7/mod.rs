1d1edbc6fbaa00eab9dac7a9070fb26902b4a485
Dylan Hicks <dman1366@gmail.com>
2020-12-06 18:57:26 -0700

    Prepare for Day 7

---
+++ src/day_7/mod.rs
@@ -0,0 +1,12 @@
+use crate::file_reader::to_string_vector;
+
+pub fn run_day_7() {
+    let file_input = to_string_vector("inputs/day_7.txt");
+
+    match file_input {
+        Ok(_input_lines) => {
+            unimplemented!()
+        }
+        Err(error) => println!("Error Parsing File: {:?}", error),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day_3;
 mod day_4;
 mod day_5;
 mod day_6;
+mod day_7;
 
 use std::io::{self, Write};
 
@@ -21,6 +22,7 @@ fn run_day(day: u32) {
         4 => day_4::run_day_4(),
         5 => day_5::run_day_5(),
         6 => day_6::run_day_6(),
+        7 => day_7::run_day_7(),
         _ => unimplemented!("I haven't done that day yet :("),
     }
 }
