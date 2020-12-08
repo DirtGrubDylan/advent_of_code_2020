8fd0169201ee93b29cc2feddaa08f8c47c5c7f36
Dylan Hicks <dman1366@gmail.com>
2020-12-08 10:01:54 -0700

    Finish Day 8!

--- src/day_8/mod.rs
+++ src/day_8/mod.rs
@@ -1,12 +1,27 @@
+mod handheld;
 
+use handheld::BootCode;
 use crate::file_reader::to_string_vector;
 
 pub fn run_day_8() {
     let file_input = to_string_vector("inputs/day_8.txt");
 
     match file_input {
-        Ok(_input_lines) => {
-            unimplemented!()
+        Ok(input_lines) => {
+            let mut boot_code = BootCode::new(&input_lines);
+
+            boot_code.execute();
+
+            let part_1 = boot_code.get_accumulator();
+
+            boot_code.reset();
+
+            boot_code.execute_self_correcting();
+
+            let part_2 = boot_code.get_accumulator();
+
+            println!("Day 8 Part 1: {}", part_1);
+            println!("Day 8 Part 2: {}", part_2);
         }
         Err(error) => println!("Error Parsing File: {:?}", error),
     };
