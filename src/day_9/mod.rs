94daf961ccce301bea1590b6e148b0a955f30bce
Dylan Hicks <dman1366@gmail.com>
2020-12-09 09:22:35 -0700

    Finish Day 9!

--- src/day_9/mod.rs
+++ src/day_9/mod.rs
@@ -1,11 +1,20 @@
+mod xmas;
+
+use xmas::Xmas;
 use crate::file_reader::to_string_vector;
 
 pub fn run_day_9() {
-    let file_input = to_string_vector("inputs/day_8.txt");
+    let file_input = to_string_vector("inputs/day_9.txt");
 
     match file_input {
-        Ok(_input_lines) => {
-            unimplemented!()
+        Ok(input_lines) => {
+            let xmas = Xmas::new(&input_lines, 25);
+
+            let part_1 = xmas.find_first_invalid_element();
+            let part_2 = xmas.find_encryption_weakness();
+
+            println!("Day 9 Part 1: {:?}", part_1);
+            println!("Day 9 Part 2: {:?}", part_2);
         }
         Err(error) => println!("Error Parsing File: {:?}", error),
     };
