a332be4fb93681bd58f437ef1754290f701b68a4
Dylan Hicks <dman1366@gmail.com>
2020-12-13 12:47:32 -0700

    Prepare for Day 14!

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day_10;
 mod day_11;
 mod day_12;
 mod day_13;
+mod day_14;
 mod day_2;
 mod day_3;
 mod day_4;
@@ -35,6 +36,7 @@ fn run_day(day: u32) {
         11 => day_11::run_day_11(),
         12 => day_12::run_day_12(),
         13 => day_13::run_day_13(),
+        14 => day_14::run_day_14(),
         _ => unimplemented!("I haven't done that day yet :("),
     }
 }
