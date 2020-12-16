8f19588561aebb0103f01c6167eef4bb537bc8fb
Dylan Hicks <dirtgrub.dylanhicks@gmail.com>
2020-12-16 03:50:10 +0000

    Finish Day 125.

--- src/day_15/mod.rs
+++ src/day_15/mod.rs
@@ -1,12 +1,26 @@
-use crate::util::file_reader::to_string_vector;
+mod memory_game;
+
+use memory_game::MemoryGame;
 
 pub fn run_day_15() {
-    let file_input = to_string_vector("inputs/day_15.txt");
+    let input: Vec<String> = vec![
+        String::from("1"),
+        String::from("20"),
+        String::from("8"),
+        String::from("12"),
+        String::from("0"),
+        String::from("14"),
+    ];
 
-    match file_input {
-        Ok(_input_lines) => {
-            unimplemented!();
-        }
-        Err(error) => println!("Error Parsing File: {:?}", error),
-    };
+    let mut game = MemoryGame::new(&input);
+
+    game.play_to_turn(2020);
+
+    let part_1 = game.get_last_number_said();
+
+    game.play_to_turn(30_000_000);
+
+    let part_2 = game.get_last_number_said();
+    println!("Day 15 Part 1: {}", part_1);
+    println!("Day 15 Part 2: {}", part_2);
 }
