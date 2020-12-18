8ceae6cdb2820745f32ee6a73908a5a36752bc5c
Dylan Hicks <dman1366@gmail.com>
2020-12-18 11:42:41 -0700

    Finish Day 17!

--- src/day_17/pocket_dimension.rs
+++ src/day_17/pocket_dimension.rs
@@ -105,7 +105,8 @@ impl PocketDimension {
             let number_of_nearby_active_cubes = self.number_of_active_cubes_around(*point);
 
             let three_active_cubes = number_of_nearby_active_cubes == 3;
-            let two_or_three_active_cubes = number_of_nearby_active_cubes == 2 || three_active_cubes;
+            let two_or_three_active_cubes =
+                number_of_nearby_active_cubes == 2 || three_active_cubes;
 
             if three_active_cubes && !cube.is_active() {
                 points_to_switch_state.push(*point);
@@ -117,6 +118,16 @@ impl PocketDimension {
         for point in points_to_switch_state {
             self.grid.get_mut(&point).unwrap().switch_state();
         }
+    }
+
+    fn make_empty_copy(&self) -> PocketDimension {
+        let mut grid_copy = self.grid.clone();
+
+        for cube in grid_copy.values_mut() {
+            cube.current_active_state = false;
+        }
+
+        PocketDimension { grid: grid_copy }
     }
 
     fn expand_grid(&mut self) {
@@ -140,11 +151,141 @@ impl PocketDimension {
             .count()
     }
 
+    fn number_of_active_cubes_around_and_including(&self, point: Point3d<i32>) -> usize {
+        self.get_nearby_locations_around_and_including(point)
+            .iter()
+            .filter(|p| self.grid.contains_key(p))
+            .map(|p| self.grid.get(p).unwrap())
+            .filter(|c| c.is_active())
+            .count()
+    }
+
     fn get_nearby_locations_around(&self, point: Point3d<i32>) -> Vec<Point3d<i32>> {
         Self::NEARBY_POINTS_OFFSET
             .iter()
             .map(|offset| point + Point3d::new(offset.0, offset.1, offset.2))
             .collect()
+    }
+
+    fn get_nearby_locations_around_and_including(&self, point: Point3d<i32>) -> Vec<Point3d<i32>> {
+        let mut result: Vec<Point3d<i32>> = Self::NEARBY_POINTS_OFFSET
+            .iter()
+            .map(|offset| point + Point3d::new(offset.0, offset.1, offset.2))
+            .collect();
+
+        result.push(point);
+
+        result
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PocketDimensions {
+    dimensions: HashMap<i32, PocketDimension>,
+    current_cycle: i32,
+}
+
+impl PocketDimensions {
+    pub fn new(info: &[String]) -> PocketDimensions {
+        let mut dimensions = HashMap::new();
+        let current_cycle = 0;
+
+        dimensions.insert(0, PocketDimension::new(info));
+
+        PocketDimensions {
+            dimensions,
+            current_cycle,
+        }
+    }
+
+    pub fn number_of_active_cubes(&self) -> usize {
+        self.dimensions
+            .values()
+            .fold(0, |acc, dimension| acc + dimension.number_of_active_cubes())
+    }
+
+    pub fn run_cycle_to(&mut self, cycle: i32) {
+        while self.current_cycle != cycle {
+            self.run_cycle();
+
+            self.current_cycle += 1;
+        }
+    }
+
+    pub fn run_cycle(&mut self) {
+        self.expand_dimensions();
+
+        let mut dimension_points_to_switch_state: Vec<(i32, Vec<Point3d<i32>>)> = Vec::new();
+
+        for (w, dimension) in self.dimensions.iter() {
+            let mut points_to_switch_state: Vec<Point3d<i32>> = Vec::new();
+
+            for (point, cube) in dimension.grid.iter() {
+                let number_of_nearby_active_cubes =
+                    self.number_of_active_cubes_around_and_including(*point, *w);
+
+                let three_active_cubes = number_of_nearby_active_cubes == 3;
+                let two_or_three_active_cubes =
+                    number_of_nearby_active_cubes == 2 || three_active_cubes;
+
+                if three_active_cubes && !cube.is_active() {
+                    points_to_switch_state.push(*point);
+                } else if !two_or_three_active_cubes && cube.is_active() {
+                    points_to_switch_state.push(*point);
+                }
+            }
+
+            dimension_points_to_switch_state.push((*w, points_to_switch_state));
+        }
+
+        for (dimension, points_to_switch) in dimension_points_to_switch_state {
+            for point in points_to_switch {
+                self.dimensions
+                    .get_mut(&dimension)
+                    .unwrap()
+                    .grid
+                    .get_mut(&point)
+                    .unwrap()
+                    .switch_state();
+            }
+        }
+    }
+
+    fn expand_dimensions(&mut self) {
+        for dimension in self.dimensions.values_mut() {
+            dimension.expand_grid();
+        }
+
+        self.dimensions.insert(
+            -self.current_cycle - 1,
+            self.dimensions.get(&0).unwrap().make_empty_copy(),
+        );
+
+        self.dimensions.insert(
+            self.current_cycle + 1,
+            self.dimensions.get(&0).unwrap().make_empty_copy(),
+        );
+    }
+
+    fn number_of_active_cubes_around_and_including(&self, point: Point3d<i32>, w: i32) -> usize {
+        let mut result = 0;
+
+        // left
+        if let Some(dimension) = self.dimensions.get(&(w - 1)) {
+            result += dimension.number_of_active_cubes_around_and_including(point);
+        }
+
+        // right
+        if let Some(dimension) = self.dimensions.get(&(w + 1)) {
+            result += dimension.number_of_active_cubes_around_and_including(point);
+        }
+
+        // self
+        if let Some(dimension) = self.dimensions.get(&w) {
+            result += dimension.number_of_active_cubes_around(point);
+        }
+
+        result
     }
 }
 
@@ -153,7 +294,6 @@ mod tests {
     use super::*;
 
     const TEST_DATA: [&str; 3] = [".#.", "..#", "###"];
-    const TEST_DATA_2: [&str; 5] = [".....", "..#..", "...#.", ".###.", "....."];
     const TEST_DATA_3: [&str; 5] = [".....", ".....", ".#.#.", "..##.", "..#.."];
 
     const TEST_POINT_2: Point3d<i32> = Point3d { x: 1, y: 1, z: -1 };
@@ -327,11 +467,30 @@ mod tests {
         assert_eq!(result, expected);
     }
 
+    #[test]
+    fn test_pocket_dimensions_number_of_active_cubes() {
+        let input = str_slice_to_string_vec(&TEST_DATA);
+
+        let mut dimensions = PocketDimensions::new(&input);
+
+        dimensions.run_cycle_to(6);
+
+        let result = dimensions.number_of_active_cubes();
+
+        let expected = 848;
+
+        assert_eq!(result, expected);
+    }
+
     fn str_slice_to_string_vec(slice: &[&str]) -> Vec<String> {
         slice.iter().map(|s| s.to_string()).collect()
     }
 
     fn get_z_slice(pocket_dimension: &PocketDimension, z: i32) -> Vec<(&Point3d<i32>, &Cube)> {
-        pocket_dimension.grid.iter().filter(|(p, _)| p.z == z).collect()
+        pocket_dimension
+            .grid
+            .iter()
+            .filter(|(p, _)| p.z == z)
+            .collect()
     }
 }
