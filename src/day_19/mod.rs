9ec71f048716135a964f0cc984b32948e55d2d35
Dylan Hicks <dman1366@gmail.com>
2020-12-20 18:21:57 -0700

    Finish Day 19!

--- src/day_19/mod.rs
+++ src/day_19/mod.rs
@@ -1,11 +1,20 @@
+mod messages;
+
 use crate::util::file_reader::to_string_vector;
+use messages::RulesAndMessages;
 
 pub fn run_day_19() {
     let file_input = to_string_vector("inputs/day_19.txt");
 
     match file_input {
-        Ok(_input_lines) => {
-            unimplemented!()
+        Ok(input_lines) => {
+            let rules_and_message = RulesAndMessages::new(&input_lines);
+
+            let part_1 = rules_and_message.number_of_valid_messages_for_rule(0);
+            let part_2 = rules_and_message.updated_number_of_valid_messages_for_rule0();
+
+            println!("Day 19 Part 1: {}", part_1);
+            println!("Day 19 Part 2: {}", part_2);
         }
         Err(error) => println!("Error Parsing File: {:?}", error),
     };
